2d337cc7cae5accef2da0591808ff35452cd8a50
Mario Martinez <mariomartinez678abc@gmail.com>
2024-01-22 15:58:43 -0500

    Agregando rutas GET a User

--- api/src/routes/usersRoutes/getUser.js
+++ api/src/routes/usersRoutes/getUser.js
@@ -5,12 +5,30 @@ const {
 const {
   banedUser,
 } = require("../../controllers/usersController/putUserController");
+const {
+  allUsers,
+  getUser,
+  getUserEmail,
+  getUserRol,
+} = require("../../controllers/usersController/getUserController");
 
 const router = Router();
 
-router.get("/user", async (req, res) => {
-  console.log("Soy el get");
-});
+// router.get("/user", async (req, res) => {
+//   console.log("Soy el get");
+// });
+
+// Ruta GET para todos los usuarios
+router.get("/user", allUsers);
+
+// Ruta GET para traer un usuario por su ID
+router.get("/user/:id", getUser);
+
+// Ruta GET para obtener información de un usuario por su correo electrónico.
+router.get("/user/:email", getUserEmail);
+
+// Ruta para verificar el rol del usuario
+router.get("/user/rol/:sub", getUserRol);
 
 // Ruta POST para el registro de los usuarios
 router.post("/user", postUser);
