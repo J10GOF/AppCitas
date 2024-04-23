474f523bf919e04c3e26d2631e922b6f46c16935
Mario Martinez <mariomartinez678abc@gmail.com>
2024-04-23 15:58:51 -0500

    Subiendo rutas

--- api/src/routes/postRoutes/getPost.js
+++ api/src/routes/postRoutes/getPost.js
@@ -1,9 +1,24 @@
 const { Router } = require("express");
+const { getAllPost, getPostByUser, myPost } = require('../../controllers/postController/getPost')
+const { createPost } = require('../../controllers/postController/postController')
+const { deletePost } = require('../../controllers/postController/deletePost')
 
 const router = Router();
 
 router.get("/post", async (req, res) => {
   console.log("Soy el get de Post");
 });
+
+router.post('/create-post', createPost)
+
+router.get("/allPost", getAllPost);
+
+router.get("/post/:userId", getPostByUser);
+
+router.get('/my-posts', myPost)
+
+//router.put("/:postId", updatePost);
+
+router.delete("/:postId", deletePost);
 
 module.exports = router;
